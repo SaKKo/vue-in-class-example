f8540dac51c8311832f9bc71b369c98825d07ec3
sakko <saklism@gmail.com>
2017-02-27 11:36:51 +0700

    add files

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="app">
+
+    <md-toolbar>
+      <md-button class="md-icon-button" @click.native="toggleLeftSidenav">
+        <md-icon>menu</md-icon>
+      </md-button>
+
+      <h2 class="md-title">My App</h2>
+    </md-toolbar>
+
+    <div>
+      <md-button class="md-raised md-accent" @click.native="toggleRightSidenav">Toggle right</md-button>
+      <p>Open console to see the events</p>
+    </div>
+
+    <md-sidenav class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
+      <md-toolbar class="md-large">
+        <div class="md-toolbar-container">
+          <h3 class="md-title">Sidenav content</h3>
+        </div>
+      </md-toolbar>
+
+      <md-list>
+        <md-list-item>
+          <router-link :to="{ name: 'Posts' }">
+            <md-icon>shopping_basket</md-icon>
+            <span>Posts</span>
+          </router-link>
+        </md-list-item>
+        <md-list-item>
+          <router-link :to="{ name: 'Hello' }">
+            <md-icon>shopping_basket</md-icon>
+            <span>Hello</span>
+          </router-link>
+        </md-list-item>
+      </md-list>
+    </md-sidenav>
+
+    <md-sidenav class="md-right" ref="rightSidenav" @open="open('Right')" @close="close('Right')">
+      <md-toolbar>
+        <div class="md-toolbar-container">
+          <h3 class="md-title">Sidenav content</h3>
+        </div>
+      </md-toolbar>
+
+      <md-button class="md-raised md-accent" @click.native="closeRightSidenav">Close</md-button>
+    </md-sidenav>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  methods: {
+    toggleLeftSidenav () {
+      this.$refs.leftSidenav.toggle()
+    },
+    toggleRightSidenav () {
+      this.$refs.rightSidenav.toggle()
+    },
+    closeRightSidenav () {
+      this.$refs.rightSidenav.close()
+    },
+    open (ref) {
+      console.log('Opened: ' + ref)
+    },
+    close (ref) {
+      console.log('Closed: ' + ref)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Posts.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="posts">
+    <h1>{{ msg }}</h1>
+    <h2>Posts</h2>
+    <ol>
+      <li v-for="post in posts">
+        {{ post.name }}
+        <br>
+        {{ post.content }}
+      </li>
+    </ol>
+    <p v-if="seen">Now you see me</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'posts',
+  data () {
+    return {
+      seen: true,
+      msg: 'Welcome to Your Vue.js App',
+      posts: [{
+        name: 'one',
+        content: 'one content'
+      },
+      {
+        name: 'two',
+        content: 'two content'
+      }]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+  color: red;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>

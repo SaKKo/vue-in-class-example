71f83453e64f95244656ecd0f98dba3ea8a07aa5
sakko <saklism@gmail.com>
2017-03-06 08:44:04 +0700

    move sidebad to new component

---
+++ src/components/LeftNavigation.vue
@@ -0,0 +1,51 @@
+<template lang="html">
+  <div class="left-navigation">
+    <md-toolbar>
+      <md-button class="md-icon-button" @click.native="toggleLeftSidenav">
+        <md-icon>menu</md-icon>
+      </md-button>
+      <h2 class="md-title">My App</h2>
+    </md-toolbar>
+    <md-sidenav class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
+      <md-toolbar class="md-large">
+        <div class="md-toolbar-container">
+          <h3 class="md-title">Sidenav content</h3>
+        </div>
+      </md-toolbar>
+      <md-list>
+        <md-list-item>
+          <router-link :to="{ name: 'Posts' }">
+            <md-icon>shopping_basket</md-icon>
+            <span>Posts</span>
+          </router-link>
+        </md-list-item>
+        <md-list-item>
+          <router-link :to="{ name: 'Hello' }">
+            <md-icon>shopping_basket</md-icon>
+            <span>Hello</span>
+          </router-link>
+        </md-list-item>
+      </md-list>
+    </md-sidenav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'left-navigation',
+  methods: {
+    toggleLeftSidenav () {
+      this.$refs.leftSidenav.toggle()
+    },
+    open (ref) {
+      console.log('Opened: ' + ref)
+    },
+    close (ref) {
+      console.log('Closed: ' + ref)
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
